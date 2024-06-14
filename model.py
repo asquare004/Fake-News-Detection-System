00cf9b2ffd9670e1360de42702900df25c7c07e2
Satyam1942 <satyamjha790@gmail.com>
2024-06-14 19:45:13 +0530

    made changes in model

--- model.py
+++ model.py
@@ -1,23 +1,22 @@
+import numpy as np
 import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
 import pickle
-import sklearn
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import TfidfVectorizer
-import text_preprocessing
 
-
-
-def predict(X, model):
-    if(model.predict(X)):
+def predict(test_prediction_classes):
+    if(test_prediction_classes[0][0]):
         return "Real News"
     else:
-       return "Fake News"
+        return "Fake News"
 
 
 def is_fake_news(news):
-    vectorizer = pickle.load(open("vectorizer.sav", "rb"))
-    trained_model = pickle.load(open("trained_model.sav", "rb"))
-    text = news
-    data = pd.DataFrame({"final" : [text_preprocessing.preprocess_text(text)]})
-    X = vectorizer.transform(data["final"]).toarray()
-    return predict(X, trained_model)
+    model = load_model("cnn_lstm.h5")
+    tokenizer = pickle.load(open("tokenizer.sav", "rb"))
+    test_sequences = tokenizer.texts_to_sequences(news)
+    padded_test = pad_sequences(test_sequences,maxlen = 42, padding = 'post', truncating = 'post')
+    test_predictions = model.predict(padded_test)
+    test_prediction_classes = (test_predictions > 0.5).astype("int32")
+    return predict(test_prediction_classes)
